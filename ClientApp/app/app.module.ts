49d31087ecccd56140f02e7269c67f79b9f84b1c
Pieter Engelbrecht <pgengel@gmail.com>
2017-05-06 21:40:04 +0200

    Added Make, model and features components, services and html.

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,11 +1,16 @@
-import { MakeService } from './services/make.service';
+import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+
+import { FeatureService } from './services/feature.service';
+import { MakeService } from './services/make.service';
+
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
@@ -20,6 +25,7 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
         VehicleFormComponent
     ],
     imports: [
+        FormsModule,
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -31,7 +37,8 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
         ])
     ],
     providers: [
-        MakeService
+        MakeService,
+        FeatureService
     ]
 })
 export class AppModule {
--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,3 +1,4 @@
+import { FeatureService } from './../../services/feature.service';
 import { MakeService } from './../../services/make.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,16 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
-  makes;
+  makes : any[];
+  models : any[];
+  features : any[];
+  vehicle : any = {};
 
-  constructor(private makeService: MakeService) { }
+  constructor(private makeService: MakeService, 
+              private featureService: FeatureService) { }
 
   ngOnInit() {
     this.makeService.getMakes().subscribe(makes => {
       this.makes = makes;
       console.log("Makes:", this.makes);
     });
+
+    this.featureService.getFeatures().subscribe(features => {
+      this.features = features;
+      console.log("Features:", this.makes);
+    });
     
+  }
+
+  onMakeChange(){
+    var selectedMake = this.makes.find(m => m.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
   }
 
 }
