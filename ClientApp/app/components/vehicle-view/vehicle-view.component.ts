816127468d167a3d1228457101247b90580d450f
Pieter Engelbrecht <pgengel@gmail.com>
2017-05-20 14:02:15 +0200

    SyntaxError: Unexpected token < in JSON at position 0

--- ClientApp/app/components/vehicle-view/vehicle-view.component.ts
+++ ClientApp/app/components/vehicle-view/vehicle-view.component.ts
@@ -2,7 +2,7 @@ import { ProgressService } from './../../services/progress.service';
 import { PhotoService } from './../../services/photo.service';
 import { ToastyService } from 'ng2-toasty';
 import { VehicleService } from './../../services/vehicle.service';
-import {ViewChild, ElementRef,  Component,   OnInit, NgZone} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -17,17 +17,17 @@ export class VehicleViewComponent implements OnInit {
 
   constructor(
     private zone: NgZone,
-    private photoService: PhotoService,
     private route: ActivatedRoute, 
     private router: Router,
     private toasty: ToastyService,
-    private vehicleService: VehicleService,
-    private progressService: ProgressService,) { 
+    private progressService: ProgressService,
+    private photoService: PhotoService,
+    private vehicleService: VehicleService) { 
 
     route.params.subscribe(p => {
       this.vehicleId = +p['id'];
       if (isNaN(this.vehicleId) || this.vehicleId <= 0) {
-        router.navigate(['/vehicles']);
+        router.navigate(['/vehicle']);
         return; 
       }
     });
@@ -42,7 +42,7 @@ export class VehicleViewComponent implements OnInit {
         v => this.vehicle = v,
         err => {
           if (err.status == 404) {
-            this.router.navigate(['/vehicles']);
+            this.router.navigate(['/vehicle']);
             return; 
           }
         });
@@ -52,13 +52,12 @@ export class VehicleViewComponent implements OnInit {
     if (confirm("Are you sure?")) {
       this.vehicleService.deleteVehicle(this.vehicle.id)
         .subscribe(x => {
-          this.router.navigate(['/vehicles']);
+          this.router.navigate(['/vehicle']);
         });
     }
   }
 
-  uploadPhoto() {
-    
+  uploadPhoto() {    
     this.progressService.startTracking()
       .subscribe(progress => {
         this.zone.run(() => {
